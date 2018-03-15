395ad8d6324583fbcee8408d386c39299f17dff2
rahul <errahuldiware@gmail.com>
2018-03-15 18:16:16 +0530

    Front End done

---
+++ FrontEnd_OnlineShoppingProject/src/main/java/com/rd/controller/PageController.java
@@ -0,0 +1,34 @@
+package com.rd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index(@RequestParam(name = "logout", required = false) String logout) {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
+		return mv;
+	}// index
+
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}// about
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}// contact
+
+}
